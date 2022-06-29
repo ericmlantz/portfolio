ea731cb4745087410dd4be0430de3e265350ad3a
ericmlantz <ericmlantz@gmail.com>
2022-06-29 11:24:48 -0400

    updated resume download link

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -10,6 +10,7 @@ const resume = () => {
           <div className='circle green'></div>
         </div>
         <div className='overflow-auto'>
+          {/* Link updated on Jun 29th, 2022 */}
         <iframe className='iframe-resume' src="https://drive.google.com/file/d/130_WhRoDZPiPWsLwOlQ8LSlencaKPqb2/preview?usp=embed_googleplus"></iframe>
         {/* https://drive.google.com/file/d/1doUvvB-4S7tVMyYJZOvumgUBv5wbPYtl/preview?usp=embed_googleplus" */}
         </div>
@@ -23,6 +24,7 @@ const resume = () => {
         </div>
       <div className='download-png'>
           <p className='download-button-text'>Click to Download Resume!</p>
+          {/* Link updated on Jun 29th, 2022 */}
           <a className="btn-primary btn download-resume" href='https://drive.google.com/uc?id=130_WhRoDZPiPWsLwOlQ8LSlencaKPqb2&export=download' role="button">Download</a>
       </div>
     </div>
